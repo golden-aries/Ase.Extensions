763c4ec2963e912142d33fed4d0808cb5dab7d92
Golden Aries <dev@goldenaries.club>
2022-10-28 11:23:36 -0700

    publish script and default feed

---
+++ Ase.Extensions.Http/publish.ps1
@@ -0,0 +1,7 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+dotnet pack --configuration Release
+
+# defaultPushSource  key should be defined in Nuget.config for the following command to work
+dotnet nuget push bin/Release/Ase.Extensions.Http.1.0.3.nupkg --api-key AzureDevOps
