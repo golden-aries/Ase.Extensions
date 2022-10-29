014ba809e1e3ffe6d00c6925c0d87cb3ef49ddf4
Golden Aries <dev@goldenaries.club>
2022-10-28 20:44:18 -0700

    publish StJson

---
+++ Ase.Extensions.Http.StJson/publish.ps1
@@ -0,0 +1,7 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+dotnet pack --configuration Release
+
+# defaultPushSource  key should be defined in Nuget.config for the following command to work
+dotnet nuget push bin/Release/Ase.Extensions.Http.StJson.1.0.6.nupkg --api-key AzureDevOps
