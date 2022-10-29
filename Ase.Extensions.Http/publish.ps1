3a502a01d629dc48a0ae5db2d46cd4c6fbcadfb9
Golden Aries <dev@goldenaries.club>
2022-10-28 20:41:45 -0700

    publishing json formatter

--- Ase.Extensions.Http/publish.ps1
+++ Ase.Extensions.Http/publish.ps1
@@ -4,4 +4,4 @@ $ErrorActionPreference = 'Stop'
 dotnet pack --configuration Release
 
 # defaultPushSource  key should be defined in Nuget.config for the following command to work
-dotnet nuget push bin/Release/Ase.Extensions.Http.1.0.3.nupkg --api-key AzureDevOps
+dotnet nuget push bin/Release/Ase.Extensions.Http.1.0.5.nupkg --api-key AzureDevOps
